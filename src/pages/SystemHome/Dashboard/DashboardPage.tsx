d2da488411a2f25558235e7dfcbef6d90ae1e77a
abdulboriy6863 <abdulboriy6863@gmail.com>
2026-08-05 11:03:39 +0900

    refactor(systemHome): remove empty DashboardPage.module.scss and clean up SCSS imports

--- src/pages/SystemHome/Dashboard/DashboardPage.tsx
+++ src/pages/SystemHome/Dashboard/DashboardPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import styles from '../../../scss/systemHome/DashboardPage.module.scss';
 import { OverviewDashboard } from '../../../libs/components/overview/OverviewDashboard';
 import type { DashboardPageProps } from '../../../libs/types/pages/pageProps';
 
 export const DashboardPage: React.FC<DashboardPageProps> = ({
   onNavigateMonitoring,
 }) => (
-  <section className={styles.page} aria-label="System Home Dashboard">
+  <section aria-label="System Home Dashboard">
     <OverviewDashboard
       onNavigateMonitoring={onNavigateMonitoring}
     />
